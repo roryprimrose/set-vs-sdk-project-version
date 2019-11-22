0ee7fddf7845adb0ab6db8e084d4545000b7e0f2
Rory Primrose <rory@neovolve.com>
2019-11-22 14:59:24 +1100

    Updated version parameters to be optional

--- version-projects.ps1
+++ version-projects.ps1
@@ -1,10 +1,22 @@
 param (
     [Parameter(Mandatory=$true)][string]$projectFilter,
-    [Parameter(Mandatory=$true)][string]$version,
-    [Parameter(Mandatory=$true)][string]$assemblyVersion,
-    [Parameter(Mandatory=$true)][string]$fileVersion,
-    [Parameter(Mandatory=$true)][string]$informationalVersion
+    [Parameter(Mandatory=$false)][string]$version,
+    [Parameter(Mandatory=$false)][string]$assemblyVersion,
+    [Parameter(Mandatory=$false)][string]$fileVersion,
+    [Parameter(Mandatory=$false)][string]$informationalVersion
 )
+
+# Check that we have a project filter
+if ([string]::IsNullOrWhiteSpace($projectFilter))
+{
+    throw "At least one version value must be supplied. Add an input parameter for either version, assemblyVersion, fileVersion or informationalVersion."
+}
+
+# Check that we have at least one version variable
+if ([string]::IsNullOrWhiteSpace($version) -or [string]::IsNullOrWhiteSpace($assemblyVersion) -or [string]::IsNullOrWhiteSpace($fileVersion) -or [string]::IsNullOrWhiteSpace($informationalVersion))
+{
+    throw "At least one version value must be supplied. Add an input parameter for either version, assemblyVersion, fileVersion or informationalVersion."
+}
 
 $sourcesDirectory = $PSScriptRoot
 
@@ -28,10 +40,27 @@ Function Set-NodeValue($rootNode, [string]$nodeName, [string]$value)
 }
 
 Write-Output "Updating project files with the following version information"
-Write-Output "Version: $version"
-Write-Output "AssemblyVersion: $assemblyVersion"
-Write-Output "FileVersion: $fileVersion"
-Write-Output "InformationalVersion: $informationalVersion"
+
+if (-not [string]::IsNullOrWhiteSpace($version))
+{
+    Write-Output "Version: $version"
+}
+
+if (-not [string]::IsNullOrWhiteSpace($assemblyVersion))
+{
+    Write-Output "AssemblyVersion: $assemblyVersion"
+}
+
+if (-not [string]::IsNullOrWhiteSpace($fileVersion))
+{
+    Write-Output "FileVersion: $fileVersion"
+}
+
+if (-not [string]::IsNullOrWhiteSpace($informationalVersion))
+{
+    Write-Output "InformationalVersion: $informationalVersion"
+}
+
 Write-Output ""
 Write-Output "Searching for projects ($projectFilter) under $sourcesDirectory"
 Write-Output ""
@@ -45,10 +74,25 @@ Get-ChildItem -Path $sourcesDirectory -Filter $projectFilter -Recurse -File |
         $projectPath = $_.FullName
         $project = Select-Xml $projectPath -XPath "//Project"
         
-        Set-NodeValue $project "Version" $version
-        Set-NodeValue $project "AssemblyVersion" $assemblyVersion
-        Set-NodeValue $project "FileVersion" $fileVersion
-        Set-NodeValue $project "InformationalVersion" $informationalVersion 
+        if (-not [string]::IsNullOrWhiteSpace($version))
+        {
+            Set-NodeValue $project "Version" $version
+        }
+
+        if (-not [string]::IsNullOrWhiteSpace($assemblyVersion))
+        {
+            Set-NodeValue $project "AssemblyVersion" $assemblyVersion
+        }
+
+        if (-not [string]::IsNullOrWhiteSpace($fileVersion))
+        {
+            Set-NodeValue $project "FileVersion" $fileVersion
+        }
+
+        if (-not [string]::IsNullOrWhiteSpace($informationalVersion))
+        {
+            Set-NodeValue $project "InformationalVersion" $informationalVersion
+        }
 
         $document = $project.Node.OwnerDocument
         $document.PreserveWhitespace = $true
