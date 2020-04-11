b4bf7b105c492e3968ae58b0fe2c422dc1332bb6
Rory Primrose <rory@neovolve.com>
2020-04-11 14:24:04 +1000

    Added first attempt at js action

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+try {
+    const projectFilter = core.getInput('projectFilter');
+    const version = core.getInput('version');
+    const assemblyVersion = core.getInput('assemblyVersion');
+    const fileVersion = core.getInput('fileVersion');
+    const informationalVersion = core.getInput('informationalVersion');
+
+    exec.exec('pwsh', ['version-projects.ps1', projectFilter, version, assemblyVersion, fileVersion, informationalVersion]);
+} catch (error) {
+    core.setFailed(error.message);
+}
