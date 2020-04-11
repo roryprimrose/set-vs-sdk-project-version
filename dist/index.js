d2fdd6ee29243318bd71cf46e08babddb56f85b1
Rory Primrose <rory@neovolve.com>
2020-04-11 14:54:03 +1000

    trying dir instead of ls

--- dist/index.js
+++ dist/index.js
@@ -968,7 +968,7 @@ async function run() {
           }
         };
 
-        await exec.exec('ls', options);
+        await exec.exec('dir', options);
 
         var result = await exec.exec('pwsh', ['-File', 'version-projects.ps1', projectFilter, version, assemblyVersion, fileVersion, informationalVersion], options);
 
