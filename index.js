f1c3175c27556adf921dc37ec932a116e0020753
Rory Primrose <rory@neovolve.com>
2020-04-11 14:37:27 +1000

    Added package step and dist output

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-    const projectFilter = core.getInput('projectFilter');
-    const version = core.getInput('version');
-    const assemblyVersion = core.getInput('assemblyVersion');
-    const fileVersion = core.getInput('fileVersion');
-    const informationalVersion = core.getInput('informationalVersion');
+function run() {
+    try {
+        const projectFilter = core.getInput('projectFilter');
+        const version = core.getInput('version');
+        const assemblyVersion = core.getInput('assemblyVersion');
+        const fileVersion = core.getInput('fileVersion');
+        const informationalVersion = core.getInput('informationalVersion');
 
-    exec.exec('pwsh', ['version-projects.ps1', projectFilter, version, assemblyVersion, fileVersion, informationalVersion]);
-} catch (error) {
-    core.setFailed(error.message);
+        exec.exec('pwsh', ['version-projects.ps1', projectFilter, version, assemblyVersion, fileVersion, informationalVersion]);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run()
