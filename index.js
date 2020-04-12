64515ebd8d897effdce8c61ff1383ce863b74eff
Rory Primrose <rory@neovolve.com>
2020-04-12 15:18:48 +1000

    Updated to use xml2js to avoid docker

--- index.js
+++ index.js
@@ -1,44 +1,96 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const glob = require('@actions/glob');
+const fs = require('fs').promises;
+const promisify = require('util').promisify;
+const xml2js = require('xml2js');
 
 async function run() {
     try {
-        const projectFilter = core.getInput('projectFilter');
-        const version = core.getInput('version');
-        const assemblyVersion = core.getInput('assemblyVersion');
-        const fileVersion = core.getInput('fileVersion');
-        const informationalVersion = core.getInput('informationalVersion');
+        const projectFilter = core.getInput('projectFilter', { required: true });
 
-        const globber = await glob.create('**')
-        
-        for await (const file of globber.globGenerator()) {
-          console.log(file)
-        }
-
-        var myError = '';
-        var myOutput = '';
-
-        const options = {};
-        options.listeners = {
-          stdout: (data) => {
-            myOutput += data.toString();
-          },
-          stderr: (data) => {
-            myError += data.toString();
-          }
+        let options = {
+            version: core.getInput('version'),
+            assemblyVersion: core.getInput('assemblyVersion'),
+            fileVersion: core.getInput('fileVersion'),
+            informationalVersion: core.getInput('informationalVersion')
         };
 
-        var result = await exec.exec('pwsh', ['-File', '../version-projects.ps1', projectFilter, version, assemblyVersion, fileVersion, informationalVersion], options);
+        core.info(`Finding projects matching ${projectFilter}`);
 
-        core.info(myOutput);
+        const globber = await glob.create(projectFilter)
 
-        if (result !== 0) {
-            core.error(myError);
-        }
-        
+        for await (const file of globber.globGenerator()) {
+            processFile(file, options);
+        }       
     } catch (error) {
         core.setFailed(error.message);
+    }
+}
+
+async function processFile(filepath, options) {
+    core.info(`Processing file ${filepath}`);
+
+    let xml = await fs.readFile(filepath);
+
+    const parser = new xml2js.Parser();
+
+    const parseString = promisify(parser.parseString);
+
+    let result = await parseString(xml);
+
+    writeVersionData(result, options);
+
+    const builder = new xml2js.Builder({headless: true});
+
+    var updatedXml = await builder.buildObject(result);
+
+    await fs.writeFile(filepath, updatedXml);
+
+    core.info(JSON.stringify(result));
+}
+
+function writeVersionData(xml, options) {
+    if (options.version) {
+        applyVersion(xml, 'Version', options.version);
+    }
+
+    if (options.assemblyVersion) {
+        applyVersion(xml, 'AssemblyVersion', options.assemblyVersion);
+    }
+
+    if (options.fileVersion) {
+        applyVersion(xml, 'FileVersion', options.fileVersion);
+    }
+
+    if (options.informationalVersion) {
+        applyVersion(xml, 'informationalVersion', options.informationalVersion);
+    }
+}
+
+function applyVersion(xml, elementName, value) {
+    let matchingElement;
+    
+    for (let propertyGroupIndex = 0; propertyGroupIndex < xml.Project.PropertyGroup.length; propertyGroupIndex++) {
+        let propertyGroup = xml.Project.PropertyGroup[propertyGroupIndex];
+
+        matchingElement = propertyGroup[elementName];
+
+        if (matchingElement) {
+            break;
+        }
+    }
+
+    if (matchingElement) {
+        core.info(`Updating PropertyGroup element ${elementName} with the value ${value}`);
+
+        matchingElement[0] = value;
+    }
+    else {
+        // There isn't an xml element with the expected name
+        core.info(`PropertyGroup element ${elementName} does not exist, adding it with the value ${value}`);
+
+        xml.Project.PropertyGroup[0][elementName] = value;
     }
 }
 
